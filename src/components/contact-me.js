a5c2a9dfb542da10c7777232da98329bb5bada15
divyanshhaagahelia <bgu298@myy.haaga-helia.fi>
2023-10-04 15:56:30 +0300

    profile_finished

--- src/components/contact-me.js
+++ src/components/contact-me.js
@@ -7,7 +7,7 @@ const Contact = () => {
     <div id="currencies-section" className="contact-area">
       <span id="contact-heading">Contact</span>
       <div className="Intro-box">
-        <text className="text-area">
+        <div className="text-area">
           Thank you for visiting my website! If you have any questions or would
           like to discuss my project further, feel free to contact me.
           <div className="profile-pic">
@@ -17,19 +17,68 @@ const Contact = () => {
               className="responsive-image"
             />
           </div>
-        </text>
+        </div>
       </div>
       <div className="contact-box">
-        Name
-        <br />
-        <input type="text"></input> Phone number<input type="text"></input>{" "}
-        <br />
-        Email<input type="text"></input> Subject<input type="text"></input>{" "}
-        <br />
-        Message
-        <input type="text"></input>{" "}
+        <form
+          action="https://formsubmit.co/divyansh72144@gmail.com"
+          method="POST"
+        >
+          <div className="input-group">
+            <div className="input-field">
+              <label htmlFor="name">Name</label>
+            </div>
+            <div className="input-field">
+              <label htmlFor="phone">Phone number</label>
+            </div>
+          </div>
+          <div className="input-group">
+            <div className="input-field">
+              <input type="text" name="name" required />
+              <input type="text" name="phone" id="phone" />
+            </div>
+          </div>
+          <div className="input-group">
+            <div className="input-field">
+              <label htmlFor="email">Email</label>
+            </div>
+          </div>
+          <div className="input-group">
+            <div className="input-field">
+              <input type="text" name="email" id="email" />
+            </div>
+          </div>
+          <div className="input-group">
+            <div className="input-field">
+              <label htmlFor="subject">Subject</label>
+            </div>
+          </div>
+          <div className="input-group">
+            <div className="input-field">
+              <input type="text" name="subject" id="subject" />
+            </div>
+          </div>
+          <div className="input-group">
+            <div className="input-field">
+              <label htmlFor="message">Message</label>
+            </div>
+          </div>
+          <div className="input-group">
+            <div className="input-field">
+              <input type="text" name="message" id="message" />
+            </div>
+          </div>
+          <div className="input-group">
+            <div className="input-field">
+              <button className="send-button" type="submit">
+                Send
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
 };
+
 export default Contact;
